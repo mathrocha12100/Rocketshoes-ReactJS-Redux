5397d874fce9a03248099824b7e5222c14f6b551
Matheus Rocha <57821927+mathrocha12100@users.noreply.github.com>
2020-03-01 00:15:08 -0300

    Add files via upload

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,20 +1,32 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { darken } from 'polished';
+
+const selectProduct = keyframes`
+  from {
+    transform: scale(1);
+  } to {
+    transform: scale(0.88);
+  }
+`;
 
 export const ProductList = styled.ul`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 20px;
   list-style: none;
-
   li {
     display: flex;
     flex-direction: column;
     background: #fff;
     border-radius: 4px;
     padding: 20px;
+    &:hover {
+      img {
+        animation: ${selectProduct} 1s forwards;
+      }
+    }
 
-    img {
+    &:u img {
       align-self: center;
       max-width: 250px;
       width: auto;
